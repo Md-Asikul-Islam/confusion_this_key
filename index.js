b9fb9fc580da77d194f975fd14aba6d9622c8190
Md-Asikul-Islam <mdasikuli48@gmail.com>
2023-03-05 08:15:41 +0600

    xxxx

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+First_part
+
 // const person = {
 //     firstName: "John",
 //     lastName : "Doe",
@@ -38,3 +40,153 @@
 
   
 // console.log(person1.fullName.call(person2)) এখানে call() মেথডএর প্যারামিটারে  বলে দিতে হবে this কে হবে । 
+
+
+
+
+// Second_part
+
+// ১। Implicit binding : পরোক্ষভাবে binding বুঝায় ।
+
+// const printPlayerNameFunction = function (obj) {
+//     obj.printPlayerName = function (){
+//         console.log(this.name);
+//     };
+// };
+
+
+// let sakib = {
+//     name : "Sakib", 
+//     age  : 35, 
+
+// };
+
+// let tamim = {
+//     name : "Tamim ", 
+//     age  : 36, 
+
+// };
+
+// printPlayerNameFunction(sakib);
+// printPlayerNameFunction(tamim);
+
+// sakib.printPlayerName();
+// tamim.printPlayerName();
+
+// *** this হচ্ছে এখানে parent element বা parent Object 
+
+// *** আমাদের খেয়াল রাখতে হবে ফাংশনটা কোথায় কল হয়েছে, সেখানে ডটের  আগে যে অবজেক্ট থাকবে ঐটাই this 
+
+// var person = function (name , age ){
+//     return { 
+//         name : name, 
+//         age  : age, 
+//         printName: function () {
+//             console.log(this.name);
+
+//         },
+//         father : {
+//             name : " Mr xxx",
+//             printName : function (){
+//                 console.log(this.name);
+//             }
+//         }
+//     };
+// };
+
+
+// var shakib = person ( "shakib" , 36);
+// shakib.printName();                                    output:::: Shakib
+
+// shakib.father.printName();                           output::::   Mr xxx
+
+
+// *** এখানেও সেইম রুল হয়েছে অর্থাৎ ডটের আগে যে অবজেক্ট থাকবে সেটাই this
+
+
+
+
+২। Explicit binding : প্রত্যক্ষভাবে  binding বুঝায় ।
+
+case: 1
+
+
+var printName = function (){
+    console.log(this.name);
+}
+
+var sakib = {
+    name: "sakib", 
+    age : 35
+};
+
+
+printName().call(sakib)
+
+এখানে আমরা চাচ্ছি printName() ফাংশন দিয়ে অবজেক্টে কল করব । যেহেতু printName() ফাংশনটা গ্লোবাল  scope আছে , টাই আমরা this দিয়ে এক্সেস করতে পারব না ।
+
+*** .call() ফাংশন প্রথম যে প্যারামিটার নিবে , সেটা একটা অবজেক্টে হবে । অর্থাৎ কোন অবজেক্টে দিয়ে আমি ফাংশনটা কল করব ।।
+
+
+case: 2   একের অধিক ্ প্যারামিটার থাকলে 
+
+
+
+*** .call() দ্বিতীয়ও প্যারামিটারে আমরা  ইচ্ছামত ভ্যালু পাস করতে পারব , একের অধিক ও দেয়া যাবে । 
+
+var v1 = "Handsome"
+
+var v2 = " Al-rounder"
+
+var v3 = " Best player "
+
+var printName = function (v1, v2, v3){
+    console.log(`${this.name} is ${v1} , ${v2} and ${v3}` );
+}
+
+var sakib = {
+    name: "sakib", 
+    age : 35
+};
+
+
+printName().call(sakib, v1, v2, v3);
+
+
+case: 3 দ্বিতীয়ও প্যারামিটারে অ্যাঁরে পাস করা এবং apply() মেথড 
+
+
+var printName = function (){
+    console.log(this.name);
+}
+
+var sakib = {
+    name: "sakib", 
+    age : 35
+};
+
+
+
+var v = ["Handsome", " Al-rounder", " Best player "]
+
+printName().apply(sakib, v)
+
+
+case: 4 bind() মেথড
+
+*** .bind() মূলত একটা ফাংশন রিটার্ন করে , অন্য মেথডের মত কল করে না , কল করতে হলে variable store করতে হবে ।
+
+*** দ্বিতীয়ও প্যারামিটারে আমরা  ইচ্ছামত ভ্যালু পাস করতে পারব , একের অধিক ও দেয়া যাবে । 
+ 
+
+
+var newFunc = printName.bind(sakib, v1, v2, var)
+
+
+৩।  new binding : 
+
+
+
+
+
+৪। window binding :
